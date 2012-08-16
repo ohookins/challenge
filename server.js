65b78edcc1b44778238ba60cb59ebc167c240369
Oliver Hookins <ohookins@gmail.com>
2012-08-17 00:24:46 +0200

    Actually provides a way to query amount of data received now.

--- server.js
+++ server.js
@@ -1,28 +1,36 @@
 var http = require('http');
 var server = http.createServer();
 var bytes = 0;
-var timerID = 0;
+var now = 0;
+
+var time = function() {
+  Math.round(Date.now()/1000);
+}
 
 var streamData = function (data) {
   bytes += data.length;
 };
 
-var printInfo = function() {
-  console.log(bytes + " bytes received");
-};
-
-var endReq = function() {
-  console.log(bytes + " bytes received by " + Math.round(Date.now()/1000));
-  bytes = 0;
-  clearInterval(timerID);
-};
+var endReq = function() {};
 
 var handleReq = function (request, response) {
-  timerID = setInterval(printInfo, 1000);
   request.on('data', streamData);
   request.on('end', endReq);
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello World\n');
+  request.setEncoding('utf8');
+  switch (request.method) {
+    case 'GET': {
+      response.writeHead(200, {'Content-Type': 'text/plain'});
+      response.end(Math.round(bytes/1024) + " Kbytes received\n");
+      break;
+    }
+    case 'POST': {
+      bytes = 0;
+      now = time();
+      response.writeHead(201);
+      response.end();
+      break;
+    }
+  }
 };
 
 server.on('request', handleReq);
